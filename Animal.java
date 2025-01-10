aa67794cb27fbaf57b64c6c727c1123576d675e8
ssamuyy <samu.yrttiaho@student.lut.fi>
2025-01-10 15:06:45 +0200

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Animal {
+    String name;
+    String species;
+    int age;
+
+    public Animal(String name, String species, int age) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+    }
+}
